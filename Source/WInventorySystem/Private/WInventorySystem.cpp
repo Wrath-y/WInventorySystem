d185813ae6da5ca469fc51be6e6729ca6f63958b
Wrath-y <805655528@qq.com>
2024-10-15 21:57:44 +0800

    Init

---
+++ Source/WInventorySystem/Private/WInventorySystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "WInventorySystem.h"
+
+#define LOCTEXT_NAMESPACE "FWInventorySystemModule"
+
+void FWInventorySystemModule::StartupModule()
+{
+    
+}
+
+void FWInventorySystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FWInventorySystemModule, WInventorySystem)
